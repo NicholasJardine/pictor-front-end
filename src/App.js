ea38e45f440b7fd27fc42e9870c1373b3b3a7cce
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-15 14:54:59 +0200

    beginning of info section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import InfoSection from './components/InfoSection';
+import { homeObjOne, homeObjTwo } from './components/InfoSection/data';
 function App() {
   return (
     <Router>
 
       <Navbar/>
-
+      <InfoSection {...homeObjOne} />
+        <InfoSection {...homeObjTwo} />
     </Router>
   );
 }
---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,35 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Forget about transaction fees',
+    description: 'Sign up with us and get access to an app that allows you to send unlimited transactions with no extra cost.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require ('../../images/awards.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText:false,
+
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'Easily accesisble from anywhere, we are the future of mobile banking in Africa. No matter where you are, We have you covered. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require ('../../images/video.svg'),
+    alt: 'piggybank',
+    dark: false,
+    primary: false,
+    darkText:true,
+
+};
