040a1fde1a93343bb1613b212ab0210630cafed9
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-16 11:35:48 +0200

    industry news imports

---
+++ src/components/News/Latest.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import Icon1 from '../../images/awards.svg'
+import Icon2 from '../../images/videos.svg'
+import Icon3 from '../../images/logo.png'
+import {LatestContainer, LatestP, LatestH2, LatestH1, LatestIcon, LatestCard, LatestWrapper} from './NewsElements'
+
+
+const Latest = () => {
+    return (
+        <LatestContainer id="Latest">
+            <LatestH1>Our Latest</LatestH1>
+            <LatestWrapper>
+
+                <LatestCard>
+
+                    <LatestIcon src={Icon1}/>
+                    <LatestH2>Reduce Expenses</LatestH2>
+                    <LatestP> We help reduce your fees and increase your overall revenue.</LatestP>
+                </LatestCard>
+
+                <LatestCard>
+                    <LatestIcon src={Icon2}/>
+                    <LatestH2>Virtual Offices</LatestH2>
+                    <LatestP> You can access our platform online anywhere in the world.</LatestP>
+                </LatestCard>
+
+                <LatestCard>
+                    <LatestIcon src={Icon3}/>
+                    <LatestH2>Premium Benefits</LatestH2>
+                    <LatestP>Unlock our special membership card that returns 5% cash back.</LatestP>
+                </LatestCard>
+
+            </LatestWrapper>
+        </LatestContainer>
+    )
+}
+
+export default Latest
--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Latest from './Latest'
+import {NewsContainer, NewsWrapper, NewsRow, Heading, Column1, Column2, TextWrapper, Subtitle, BtnWrap, TopLine, ImgWrap, Img} from './NewsElements'
 const News = () => {
     return (
         <div>
