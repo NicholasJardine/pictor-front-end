05c30d7b77271e7db6971b17b8c0d35970080c91
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-16 11:41:48 +0200

    some layout

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Latest from './Latest'
 import {NewsContainer, NewsWrapper, NewsRow, Heading, Column1, Column2, TextWrapper, Subtitle, BtnWrap, TopLine, ImgWrap, Img} from './NewsElements'
-const News = () => {
+const News = ({lightBg, img, id, imgStart, description, topLine, lightText, darkText, headline, buttonLabel, alt, primary, dark, dark2 }) => {
     return (
-        <div>
-            
-        </div>
+        <>
+          <NewsContainer>
+              
+                <NewsWrapper>
+                
+                    <NewsRow>
+
+                        <Column2>
+                        
+                        
+                        </Column2>
+
+
+                    </NewsRow>
+                </NewsWrapper>    
+          </NewsContainer>  
+        </>
     )
 }
 
