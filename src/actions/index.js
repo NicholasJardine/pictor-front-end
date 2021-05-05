9dcace34ed4d01f6244803f318275ab5b5345021
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-05 13:41:45 +0200

    puhsing before cloning

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,14 @@
 export const SELECT_USER = 'SELECT_USER'; 
 export const SET_USERS = 'SET_USERS'; 
 export const USER_CREATED = 'POST_CREATED';
-const ROOT_URL = 'https://pictor-api.herokuapp.com/api/v1/users'
+const ROOT_URL = 'http://pictor-official-api.herokuapp.com/api/v1/users'
 
 
 export function createUser(body, callback) {
- const request = fetch("https://pictor-api.herokuapp.com/users", {
+ const request = fetch("http://pictor-official-api.herokuapp.com/users", {
  method: 'POST',
- headers: { 'Content-Type': 'application/json' },
- body: JSON.stringify(body)
+ headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+ body: body
  }).then(response => response.json())
  .then(callback);
  return {
@@ -19,7 +19,7 @@ export function createUser(body, callback) {
 } 
 
 export function selectUser(id) {
-    const promise = fetch(`https://pictor-api.herokuapp.com/api/v1/users/${id}`)
+    const promise = fetch(`http://pictor-official-api.herokuapp.com/api/v1/users/${id}`)
     .then(response => response.json());
     return {
     type: SELECT_USER,
